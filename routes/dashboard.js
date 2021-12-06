5faff803dacf53c6646de6d930673d2c660cff4b
ArliArliwanh <arliarliwanh13@gmail.com>
2021-12-06 14:40:29 +0700

    commit

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,7 +3,5 @@ const route = express.Router();
 const dashboard = require("../controllers/dashboard");
 
 route.get("/dashboard/cancelbilldetaillist/:bill_id", dashboard.cancelbilldetaillist);
- 
-
-
+route.get("/dashboard/cancelbilllist/:device_code/:drawnumber", dashboard.cancelbilllist);
 module.exports = route;

915d88024e64b31ea9c163e353a56997dd653ca0
ArliArliwanh <arliarliwanh13@gmail.com>
2021-12-22 17:38:33 +0700

    commit

--- controllers/account.controller.js
+++ controllers/account.controller.js
@@ -5,25 +5,27 @@ const jwt = require('jsonwebtoken')
 let account = require('../models/account.model')
 let SQL = ``
 const logger = require('../config-log/logger')
-const process = require('../tasks')
 const bcrypt = require('bcrypt')
 require('dotenv').config()
 //.............Login......................//
 exports.login = async (req, res) => {
 
-   // const { device_code, us_pwd } = req.params
-
      account = req.params
     logger.info(`POST/api/login/${account.device_code}/${account.us_pwd}`)
+
 try{
     await connection.connect(async (err, cleint, done) => {
         if (!err) {
             let date_offline = ""
+            let date_end     = ""
+           
             const isonline =  cleint.query(`SELECT to_char("date_offline", 'DD/MM/YYYY') AS date_offline FROM tbl_online WHERE online_status = 1`)
 
             if ((await isonline).rowCount > 0) {
 
                 date_offline = (await isonline).rows[0].date_offline
+                date_end     = (await isonline).rows[0].date_end
+                time_end     = (await isonline).rows[0].time_end
 
                  SQL = `SELECT * FROM  public.tbl_user_seller WHERE  device_code = $1 AND us_dlst = true`
 
@@ -51,13 +53,13 @@ try{
                                         online: true,
                                         deviceCode: account.device_code,
                                         offlineDate: date_offline,
+                                        date_end: date_end,
                                         isOverMaxSell: false,
                                         accessToken: token,
                                         setNumberList: setnumberList
                                     });
                                 } else {
                                     return res.status(404).send({
-                                        auth: false,
                                         message: "Password incorrect!"
                                     })
 
@@ -218,15 +220,18 @@ exports.CheckVersionV2 = async (req, res) => {
     logger.info(`GET/api/account/CheckVersionV2/${account.version_name}/${account.device_imei}`)
 
     //Check device imei of devive
-    SQL = `SELECT  tbl_device.device_number, tbl_device.device_code,
+    SQL = `SELECT  tbl_device.device_number, tbl_device.device_code, tbl_branch_code.phone,
                    tbl_branch_code.branch_id, tbl_province.province_name , tbl_branch_code.branch_code
-           FROM    tbl_branch_code, tbl_province, tbl_device
-           WHERE   tbl_branch_code.province_id = tbl_province.provice_id
-             AND   tbl_branch_code.branch_id   = tbl_device.branch_id 
-             AND   tbl_device.device_imei      = $1
-             AND   tbl_device.device_dlst      = true
-             AND   tbl_branch_code.branch_dlst = true
-             `
+          FROM     tbl_branch_code, tbl_province, tbl_device, tbl_service_unit, tbl_user_seller
+            WHERE  tbl_branch_code.province_id = tbl_province.provice_id
+            AND   tbl_branch_code.branch_id   = tbl_service_unit.branch_id 
+            AND   tbl_service_unit.su_id      = tbl_user_seller.unit_id
+            AND   tbl_user_seller.device_code = tbl_device.device_code
+            AND   tbl_device.device_imei      = $1
+            AND   tbl_device.device_dlst      = true
+            AND   tbl_branch_code.branch_dlst = true
+            AND   tbl_service_unit.su_dlst    = true
+            AND   tbl_user_seller.us_dlst     = true`
 
     await connection.connect((err, cleint, done) => {
         if (!err) {
@@ -246,7 +251,8 @@ exports.CheckVersionV2 = async (req, res) => {
                         device_code: results.rows[0].device_code,
                         branch_id: results.rows[0].device_branch_id,
                         branchName: results.rows[0].province_name + " ເລກ " + results.rows[0].branch_code,
-                        isVersionLatest: isVersionLatest
+                        isVersionLatest: isVersionLatest,
+                        branchPhone: results.rows[0].phone
                     })
                 }
             })
